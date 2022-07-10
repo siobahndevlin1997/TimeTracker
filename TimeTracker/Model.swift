8df3f71c799a752979663ca5abc778f823d3075f
Siobahn Devlin <siobahndevlin97@gmail.com>
2022-07-10 19:49:55 -0400

    added boilerplate files

---
+++ TimeTracker/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  TimeTracker
+//
+//  Created by Siobahn Devlin on 7/10/22.
+//
+
+import Foundation
---
+++ TimeTracker/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  TimeTracker
+//
+//  Created by Siobahn Devlin on 7/10/22.
+//
+
+import Foundation
