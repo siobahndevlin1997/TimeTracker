f23f51529f3b07feeffcd64dc0b30b6cb465c7ca
Siobahn Devlin <siobahndevlin97@gmail.com>
2022-07-10 19:44:31 -0400

    Initial Commit

---
+++ TimeTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeTracker
+//
+//  Created by Siobahn Devlin on 7/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Siobahn Devlin on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
